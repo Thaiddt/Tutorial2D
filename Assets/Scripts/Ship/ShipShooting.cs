e3582fd751803422b7be3c874742fe9d0aaa524b
Thaiddt <110778069+Thaiddt@users.noreply.github.com>
2023-09-18 18:04:15 +0700

    Lenh if va tao dan

---
+++ Assets/Scripts/Ship/ShipShooting.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShooting : MonoBehaviour
+{
+    [SerializeField] protected bool isShooting = false;
+    [SerializeField] protected Transform bulletPrefab;
+
+    private void FixedUpdate()
+    {
+        this.Shooting();
+    }
+
+    protected virtual void Shooting()
+    {
+        if (!this.isShooting) return;
+
+        Instantiate(this.bulletPrefab);
+        Debug.Log("Shooting");
+    }
+
+}
